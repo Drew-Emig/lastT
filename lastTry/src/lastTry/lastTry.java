9d3f6d38c8582ab5f6efb75a388567bbd43ba3fa
dmemig <drew.emig@spglobal.com>
2017-12-20 17:21:19 -0700

    first commit

---
+++ lastTry/src/lastTry/lastTry.java
@@ -0,0 +1,11 @@
+package lastTry;
+
+public class lastTry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("we finally did it, thanks ryan!!!");
+
+	}
+
+}
